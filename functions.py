3740d8747c11f2f483779ff9ec320e3290b22d9f
Pablo Rivera <if721005@iteso.mx>
2023-02-05 20:54:56 -0600

    tidy up passive 1

--- functions.py
+++ functions.py
@@ -1,32 +1,38 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.8.5"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 4
-}
+
+import os
+import glob
+import pandas as pd
+import yfinance as yf
+import numpy as np
+import itertools
+from datetime import datetime
+
+def read_multiple_csv(path):
+    
+    'Función que lee multiples archivos csv descargados del NAFTRAC'
+    
+    all_files = glob.glob(os.path.join(path, "*.csv"))     # advisable to use os.path.join as this makes concatenation OS independent
+
+    df_from_each_file = [pd.read_csv(f, skiprows = 2) for f in all_files]
+
+    concatenated_df = pd.concat(df_from_each_file)
+    
+    concatenated_df.dropna(axis = 0, how = 'all', inplace = True)
+    
+    concatenated_df = concatenated_df[pd.notnull(concatenated_df['Peso (%)'])] # Quitar nulos en la columna de peso
+ 
+    return concatenated_df
+
+
+def get_constant_tickers(csv_files_df, r_ticker):
+    
+    all_tickers = csv_files_df.iloc[:,0].unique()
+    
+    c_tickers = [csv_files_df[csv_files_df['Ticker'] == i].iloc[0,0] + '.MX' for i in all_tickers if len(csv_files_df[csv_files_df['Ticker'] == i]) == 25]
+
+    c_tickers = [s.replace('*', '') for s in c_tickers]
+    c_tickers = [s.replace('LIVEPOLC.1', 'LIVEPOLC-1') for s in c_tickers]
+    c_tickers.remove(r_ticker)
+    c_tickers.sort()
+
+    return c_tickers
