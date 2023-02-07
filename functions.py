42bcc8c0455a64aec9a41d4d1789b8fbc3b1bd77
Pablo Rivera <if721005@iteso.mx>
2023-02-06 18:05:03 -0600

    functions progress

--- functions.py
+++ functions.py
@@ -6,6 +6,8 @@ import yfinance as yf
 import numpy as np
 import itertools
 from datetime import datetime
+import locale
+locale.setlocale(locale.LC_TIME, 'es_ES')
 
 def read_multiple_csv(path):
     
@@ -21,7 +23,7 @@ def read_multiple_csv(path):
     
     concatenated_df = concatenated_df[pd.notnull(concatenated_df['Peso (%)'])] # Quitar nulos en la columna de peso
  
-    return concatenated_df
+    return all_files, concatenated_df
 
 
 def get_constant_tickers(csv_files_df, r_ticker):
@@ -36,3 +38,42 @@ def get_constant_tickers(csv_files_df, r_ticker):
     c_tickers.sort()
 
     return c_tickers
+
+def get_all_dates(all_files: 'dirección de todos los archivos'):
+    
+    # Obtenemos los nombres de la primer fila de cada archivo
+    dates = [pd.read_csv(f, nrows = 1).columns for f in all_files]
+    
+    # Tomar solamente las fechas, si la columna tiene "unnamed" o "fecha" no se toma
+    dates = [[x for x in dates[j] if not 'Unnamed' in x] for j in range(len(dates))]
+    dates = [[x for x in dates[j] if not 'fecha' in x] for j in range(len(dates))]
+    
+    dates = [item for sublist in dates for item in sublist]
+
+    return dates
+
+def replace_list_value(the_list, to_replace, new_value):
+    
+    idx = the_list.index(to_replace)
+    
+    the_list[idx] = new_value
+    
+    return the_list
+
+def str_to_datetime(date: str, current_format: str, new_format: str):
+    
+    change_format = datetime.strptime(date, current_format)
+    
+    given_format = change_format.strftime(new_format)
+    
+    return given_format
+
+def get_ticker_prices(tickers, dates):
+    
+    data = [yf.download(tickers, start = i).iloc[0:1,len(tickers):len(tickers) * 2] for i in dates]
+
+    prices = pd.concat(data)
+    
+    return prices
+
+
